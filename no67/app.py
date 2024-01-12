55866f2ed68c706e2a7142f9190d0b08ecdeb3eb
Seanf122 <seanfang122@gmail.com>
2024-01-12 12:31:19 +0800

    Imporve: Preserve the nav-bar for all pages

--- no67/app.py
+++ no67/app.py
@@ -4,31 +4,31 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('home.html')  # The navigation bar should be part of this template.
+    return render_template('home.html', title='Home', heading='Home')
 
 @app.route('/page1')
 def page1():
-    return 'Page 1'
+    return render_template('home.html', title='Page 1', heading='Page 1')
 
 @app.route('/page2')
 def page2():
-    return 'Page 2'
+    return render_template('home.html', title='Page 2', heading='Page 2')
 
 @app.route('/page3')
 def page3():
-    return 'Page 3'
+    return render_template('home.html', title='Page 3', heading='Page 3')
 
 @app.route('/list/page1')
 def list_page1():
-    return 'Subpage 1'
+    return render_template('home.html', title='Subpage 1', heading='Subpage 1')
 
 @app.route('/list/page2')
 def list_page2():
-    return 'Subpage 2'
+    return render_template('home.html', title='Subpage 2', heading='Subpage 2')
 
 @app.route('/list/page3')
 def list_page3():
-    return 'Subpage 3'
+    return render_template('home.html', title='Subpage 3', heading='Subpage 3')
 
 if __name__ == '__main__':
     app.run(debug=True)
